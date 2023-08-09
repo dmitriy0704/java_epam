b3e4a3ab48bf12d602a039db1d75b050062b81f3
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2023-08-09 16:19:51 +0300

    Пройдена 1 глава

--- src/my/home/package01/Main.java
+++ src/my/home/package01/Main.java
@@ -1,4 +1,14 @@
 package my.home.package01;
 
 public class Main {
+    public static void main(String[] args) {
+        TestMessage testMessage = new TestMessage();
+        testMessage.printMsg("tutorial-> https://docs.oracle.com/javase/tutorial/");
+    }
+}
+
+class TestMessage {
+    void printMsg(String msg) {
+        System.out.println(msg);
+    }
 }
