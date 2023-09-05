93a95cb68e6589b832e9e6da2393bf25cc34d907
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2023-09-05 14:33:05 +0300

    Пройдена 1 глава

--- src/my/home/package02/Main.java
+++ src/my/home/package02/Main.java
@@ -1,7 +1,15 @@
 package my.home.package02;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-     int v = 4_200_000;
+        var v = 0;
+        String[] strings = {"a","b","c"};
+        for (; v < strings.length; v++) {
+            strings[v] = "java";
+        }
+        System.out.println(Arrays.toString(strings));
     }
 }
