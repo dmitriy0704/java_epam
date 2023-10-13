70643fe2e752161176c9bc7d0c450bf72cde7bbb
Dmitriy Folomkin <dmitriy.folomkin@yandex.ru>
2023-10-13 15:26:46 +0300

    Циклы - в процессе

--- src/my/home/package02/Main.java
+++ src/my/home/package02/Main.java
@@ -1,15 +1,25 @@
 package my.home.package02;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class Main {
-    public static void main(String[] args) {
-        var v = 0;
-        String[] strings = {"a","b","c"};
-        for (; v < strings.length; v++) {
-            strings[v] = "java";
-        }
-        System.out.println(Arrays.toString(strings));
+    public static void main(String[] args) throws IllegalAccessException {
+
+     //Циклы:
+
+
+
+
+
+    }
+
+    public static int getRole(String role) throws IllegalAccessException {
+        return switch (role) {
+            case "guest" -> 1;
+            case "client" -> 2;
+            case "moderator" -> 3;
+            case "admin" -> 4;
+            default -> {
+                throw new IllegalAccessException("non autentic role " + role);
+            }
+        };
     }
 }
